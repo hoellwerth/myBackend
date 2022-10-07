0692d44f0101c9f637fc554ba539d99bed03a786
Baumistlustig <johannes.hoellwerth@protonmail.com>
2022-10-07 22:55:09 +0200

    Added Mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import * as dotenv from 'dotenv';
+
+dotenv.config({ path: 'src/environment/dev.env' });
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot(process.env.DB_URL, { dbName: 'test' })],
   controllers: [AppController],
   providers: [AppService],
 })
