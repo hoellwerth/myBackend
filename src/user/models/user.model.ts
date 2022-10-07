0b1678cfe97d913363347aa90d9bb4ce94bb3351
Baumistlustig <johannes.hoellwerth@protonmail.com>
2022-10-07 23:04:36 +0200

    Added UserModel

---
+++ src/user/models/user.model.ts
@@ -0,0 +1,13 @@
+import * as mongoose from 'mongoose';
+
+export const UserSchema = new mongoose.Schema({
+  username: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+  authority: { type: Number, required: true },
+  permissions: { type: Array, required: true },
+  role: { type: String || Object, required: true },
+  token: { type: String, required: false },
+  bio: { type: String, required: false },
+  status: { type: String, required: false },
+});
