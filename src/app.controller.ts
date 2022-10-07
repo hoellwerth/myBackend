a6ac35a1b40507441d18e2c80dd12ba594eaea99
Baumistlustig <johannes.hoellwerth@protonmail.com>
2022-10-07 22:57:09 +0200

    Adde Throttler

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Throttle } from '@nestjs/throttler';
 
+@Throttle()
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,18 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import * as dotenv from 'dotenv';
+import { ThrottlerModule } from '@nestjs/throttler';
 
 dotenv.config({ path: 'src/environment/dev.env' });
 
 @Module({
-  imports: [MongooseModule.forRoot(process.env.DB_URL, { dbName: 'test' })],
+  imports: [
+    MongooseModule.forRoot(process.env.DB_URL, { dbName: 'test' }),
+    ThrottlerModule.forRoot({
+      ttl: 60,
+      limit: 30,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
