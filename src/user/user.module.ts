e53846f61b295e8747ce358ab7064f5e0ef79828
Baumistlustig <johannes.hoellwerth@protonmail.com>
2022-10-07 23:10:12 +0200

    Fixed import issues in UserModule

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -2,8 +2,17 @@ import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './services/user.service';
 import { RegisterService } from './services/register.service';
+import { UserSchema } from './models/user.model';
+import { SaltSchema } from './models/salt.model';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: 'User', schema: UserSchema },
+      { name: 'Salt', schema: SaltSchema },
+    ]),
+  ],
   controllers: [UserController],
   providers: [UserService, RegisterService],
 })
