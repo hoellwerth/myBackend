76853060a6fa68844f9bc9ed9a66949a72338bde
Baumistlustig <johannes.hoellwerth@protonmail.com>
2022-10-08 13:13:41 +0200

    Added JwtStrategy

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './services/auth.service';
+import { UserModule } from '../user/user.module';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './strategy/jwt.strategy';
 
 @Module({
-  providers: [AuthService],
+  imports: [
+    UserModule,
+    PassportModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '7d' },
+    }),
+  ],
+  providers: [AuthService, JwtStrategy],
 })
 export class AuthModule {}
