80e0003756b1078d651f8ab3efac58d2ba02b0ad
Baumistlustig <johannes.hoellwerth@protonmail.com>
2022-10-16 21:42:08 +0200

    Added picture upload

--- src/profile/models/profile.model.ts
+++ src/profile/models/profile.model.ts
@@ -4,9 +4,13 @@ export const ProfileSchema = new mongoose.Schema({
   userId: { type: String, required: true },
   status: { type: String, required: false },
   bio: { type: String, required: false },
+  buffer: { type: Object, required: true },
+  filename: { type: String, required: true },
 });
 
 export interface Profile {
+  buffer: object;
+  filename: string;
   userId: string;
   status: string | null;
   bio: string | null;
--- src/profile/services/profile.service.ts
+++ src/profile/services/profile.service.ts
@@ -6,6 +6,7 @@ import {
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Profile } from '../models/profile.model';
+import * as fs from 'fs';
 
 @Injectable()
 export class ProfileService {
@@ -54,5 +55,31 @@ export class ProfileService {
     profile.save();
 
     return { success: true };
+  }
+
+  // ---- Profile Picture ---- //
+
+  // Upload profile picture
+  async uploadPicture(filename: string, userId: string): Promise<any> {
+    // Get file from cache
+    const file = fs.readFileSync(`./src/profile/cache/${filename}`);
+
+    // Delete file from cache
+    fs.unlink(`./src/profile/cache/${filename}`, (err) => {
+      return err;
+    });
+
+    // get profile
+    const profile = await this.getProfile(userId);
+
+    // modify profile
+    if (profile) {
+      profile.buffer = file;
+      profile.filename = filename;
+
+      const result = await profile.save();
+
+      return { modified: result.id };
+    }
   }
 }
