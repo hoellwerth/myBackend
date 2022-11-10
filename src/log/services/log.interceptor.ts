6ee51ce2b1c0dd4334a595e91594a2ada31ae722
Baumistlustig <johannes.hoellwerth@protonmail.com>
2022-11-10 20:08:18 +0100

    Fixed Error response undefined

--- src/log/services/log.interceptor.ts
+++ src/log/services/log.interceptor.ts
@@ -32,11 +32,13 @@ export class LogInterceptor implements NestInterceptor {
             `\x1b[31m[Nest] ${
               process.pid
             }  -\x1b[0m ${new Date().toLocaleDateString()}, ${new Date().toLocaleTimeString()}`,
-            `\x1b[31m    ERROR\x1b[0m`,
+            `\x1b[31m  ERROR\x1b[0m`,
             `\x1b[33m[ExceptionHandler]\x1b[0m`,
             `\x1b[31mCalled {${context.getArgs()[0].url},`,
             `${context.getArgs()[0].method}},`,
-            `Error: {${e.response.statusCode} ${e.response.message}},`,
+            `Error: {${e.response ? e.response.statusCode : e} ${
+              e.response ? e.response.message : ''
+            }},`,
             `Client: {${context.getArgs()[0].headers['user-agent']},`,
             `${context.getArgs()[0].ip}}`,
             `\x1b[33m${Date.now() - now}ms\x1b[0m`,
