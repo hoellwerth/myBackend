edf0a88de208b597fc859402979ade6e48c62f2a
Johannes Höllwerth <johannes.hoellwerth@protonmail.com>
2022-12-01 21:19:36 +0100

    Added CommentService tests

--- src/post/services/vote.service.spec.ts
+++ src/post/services/vote.service.spec.ts
@@ -1,5 +1,43 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { VoteService } from './vote.service';
+import { Post } from '../models/post.model';
+
+export const mockPost = (
+  title = 'Test',
+  content = 'Test',
+  authorId = '636421313d2604ab7858f9a8',
+  comments: string[] = ['id'],
+  likedBy: string[] = [],
+  dislikedBy: string[] = [],
+  created = new Date(),
+  updated = null,
+  parent = 'id',
+  _id = 'id',
+): Post => ({
+  title,
+  content,
+  comments,
+  likedBy,
+  dislikedBy,
+  created,
+  updated,
+  authorId,
+  parent,
+  _id,
+});
+
+export class PostServiceMock {
+  static getPostById = jest.fn().mockResolvedValue({
+    ...mockPost(),
+    save: () => {
+      return 'id';
+    },
+  });
+  static editPost = jest.fn().mockResolvedValue(mockPost());
+  static createPost = jest.fn().mockResolvedValue(mockPost());
+  static deletePost = jest.fn().mockResolvedValue(true);
+  static getAllPosts = jest.fn().mockResolvedValue([mockPost(), mockPost()]);
+}
 
 describe('VoteService', () => {
   let service: VoteService;
